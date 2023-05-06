84c67618cc2bae577a3b0609cec406211b94c4d5
Codehash001 <codehashblockchain@gmail.com>
2023-05-06 16:11:22 +0530

    fix classes

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -1,8 +1,9 @@
+import { stringify } from "querystring";
 
 const indexname = process.env.PINECONE_INDEX_NAME
 const namespace = process.env.PINECONE_NAMESPACE
 
-const PINECONE_INDEX_NAME = indexname;
-const PINECONE_NAME_SPACE = namespace;
+const PINECONE_INDEX_NAME = `${indexname}`;
+const PINECONE_NAME_SPACE = `${namespace}`;
 
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
