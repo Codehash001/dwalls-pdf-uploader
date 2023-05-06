5262c6022773ebb31ec09a7a9017898d218b7fae
Codehash001 <codehashblockchain@gmail.com>
2023-05-06 15:57:33 +0530

    first commit

---
+++ config/pinecone.ts
@@ -0,0 +1,8 @@
+
+const indexname = process.env.PINECONE_INDEX_NAME
+const namespace = process.env.PINECONE_NAMESPACE
+
+const PINECONE_INDEX_NAME = indexname;
+const PINECONE_NAME_SPACE = namespace;
+
+export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
---
+++ utils/makechain.ts
@@ -0,0 +1,59 @@
+import { OpenAIChat } from 'langchain/llms';
+import { LLMChain, ChatVectorDBQAChain, loadQAChain } from 'langchain/chains';
+import { PineconeStore } from 'langchain/vectorstores';
+import { PromptTemplate } from 'langchain/prompts';
+import { CallbackManager } from 'langchain/callbacks';
+
+const CONDENSE_PROMPT =
+  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question. "add according to the provided context" at the end.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:`);
+
+const QA_PROMPT = PromptTemplate.fromTemplate(
+  `We have provided context information below: \n
+  ---------------------\n
+  {context}\n
+  ---------------------\n
+  Given this information, Please answer my question in the same language that I used to ask you.\n
+  Please answer the question: {question}\n
+
+=========
+Answer in Markdown:`,
+);
+
+export const makeChain = (
+  vectorstore: PineconeStore,
+  onTokenStream?: (token: string) => void,
+) => {
+  const questionGenerator = new LLMChain({
+    llm: new OpenAIChat({ temperature: 0 }),
+    prompt: CONDENSE_PROMPT,
+  });
+  const docChain = loadQAChain(
+    new OpenAIChat({
+      temperature: 0.7,
+      modelName: 'gpt-3.5-turbo', //change this to older versions (e.g. gpt-3.5-turbo) if you don't have access to gpt-4
+      streaming: Boolean(onTokenStream),
+      callbackManager: onTokenStream
+        ? CallbackManager.fromHandlers({
+            async handleLLMNewToken(token) {
+              onTokenStream(token);
+              console.log(token);
+            },
+          })
+        : undefined,
+    }),
+    { prompt: QA_PROMPT },
+  );
+
+  return new ChatVectorDBQAChain({
+    vectorstore,
+    combineDocumentsChain: docChain,
+    questionGeneratorChain: questionGenerator,
+    returnSourceDocuments: true,
+    k: 2, //number of source documents to return
+  });
+};
